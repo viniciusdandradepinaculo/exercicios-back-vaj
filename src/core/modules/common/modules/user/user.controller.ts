bc46d5040ffb54e8b3671c5b889f436449d4bfcc
viniciusdandradepinaculo <vinicius@pinaculodigital.com.br>
2025-12-03 19:08:27 -0300

    feat(user): added correct response form and documentation to findById method

--- src/core/modules/common/modules/user/user.controller.ts
+++ src/core/modules/common/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiTags } from '@nestjs/swagger';
 import { Doc } from 'src/utils/documentation/doc';
@@ -8,13 +8,20 @@ import { Doc } from 'src/utils/documentation/doc';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Doc({
+    name: 'Buscar usuários',
+    description: "Busca de todos os usuários no banco",
+    statusCode: HttpStatus.OK
+  })
   @Get('/')
   async findAll() {
     return await this.userService.findAll();
   }
 
   @Doc({
-    name: 'Find',
+    name: 'Buscar usuário por ID',
+    description: "Busca de usuário no banco via identificador único",
+    statusCode: HttpStatus.OK
   })
   @Get('/:id')
   async findById(@Param('id') id: string) {
--- src/core/modules/common/modules/user/user.service.ts
+++ src/core/modules/common/modules/user/user.service.ts
@@ -6,12 +6,31 @@ export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findById(id: string): Promise<any> {
-    return await this.prisma.user.findUnique({
-      where: { id },
-    });
+    const user = await this.prisma.user.findUnique(
+      {where:{id}, 
+      select:{id:true, 
+        email:true, 
+        createdAt:true, 
+        profile:{
+          select:{
+            bio:true,
+            birthDate:true,
+            posts: {
+              orderBy: {createdAt:'desc'},
+              select: {
+                id:true,
+                title:true,
+                content:true,
+                createdAt:true
+              }
+            }
+          }
+    }}})
+    return user;
   }
 
   async findAll(): Promise<any[]> {
-    return await this.prisma.user.findMany();
+    const users = this.prisma.user.findMany({select:{id:true, profile:{select:{username:true}}}})
+    return users;
   }
 }
